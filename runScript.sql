661ac83ca7897fa3c30c89db164485ae3c874299
Ritz6533 <riteshjungshah6533@gmail.com>
2023-01-23 02:15:19 +0000

    new procedure

---
+++ runScript.sql
@@ -0,0 +1,22 @@
+COLUMN object_name FORMAT A30;
+COLUMN object_type FORMAT A12;
+PURGE RECYCLEBIN;
+SELECT object_name, object_type FROM user_objects;
+
+-- CSY2038_181@student/CSY2038_181
+@D:\university\year_2\database\as2\CODES/createtables.sql
+@D:\university\year_2\database\as2\CODES/constraint.sql
+@D:\university\year_2\database\as2\CODES/insertData.sql
+@D:\university\year_2\database\as2\CODES/query.sql
+@D:\university\year_2\database\as2\CODES/procedure.sql
+@D:\university\year_2\database\as2\CODES/function.sql
+@D:\university\year_2\database\as2\CODES/trigger.sql
+@D:\university\year_2\database\as2\CODES/cursor.sql
+@D:\university\year_2\database\as2\CODES/drop.sql
+
+
+
+COLUMN object_name FORMAT A30;
+COLUMN object_type FORMAT A12;
+PURGE RECYCLEBIN;
+SELECT object_name, object_type FROM user_objects;
